dd997d41e53597382168a83a1c6bab9deb13e874
Yusuke Goto <sample@email.com>
2023-02-25 08:27:56 +0900

    1094292.052_136786.5065

--- submit.rs
+++ submit.rs
@@ -260,13 +260,13 @@ static mut PARAM: Param = Param {
     delta_cost_w: 0,
     atk_eval_rate: 0
 };
-// 1094953.946 136869.24325
+// 1094292.052 136786.5065
 const PARAMS: [Param; 8] = [
     Param {eff: 12, key_power: 13, key_exca_th: 51, observe_power: 15, observe_exca_th: 71, connect_power: 13, connect_exca_th: 250, evalw: 12, fix_rate: 141, delta_range: 2, delta_cost_w: 25, atk_eval_rate: 1, },
     Param {eff: 13, key_power: 16, key_exca_th: 501, observe_power: 25, observe_exca_th: 46, connect_power: 25, connect_exca_th: 110, evalw: 16, fix_rate: 51, delta_range: 1, delta_cost_w: 7, atk_eval_rate: 1, },
     Param {eff: 15, key_power: 37, key_exca_th: 501, observe_power: 30, observe_exca_th: 36, connect_power: 34, connect_exca_th: 340, evalw: 12, fix_rate: 31, delta_range: 2, delta_cost_w: 22, atk_eval_rate: 1, },
     Param {eff: 12, key_power: 34, key_exca_th: 851, observe_power: 40, observe_exca_th: 36, connect_power: 40, connect_exca_th: 300, evalw: 12, fix_rate: 151, delta_range: 2, delta_cost_w: 10, atk_eval_rate: 1, },
-    Param {eff: 12, key_power: 46, key_exca_th: 1051, observe_power: 45, observe_exca_th: 1, connect_power: 52, connect_exca_th: 230, evalw: 16, fix_rate: 206, delta_range: 1, delta_cost_w: 25, atk_eval_rate: 2, },
+    Param {eff: 13, key_power: 37, key_exca_th: 1801, observe_power: 55, observe_exca_th: 6, connect_power: 58, connect_exca_th: 220, evalw: 16, fix_rate: 206, delta_range: 1, delta_cost_w: 28, atk_eval_rate: 2, },
     Param {eff: 13, key_power: 70, key_exca_th: 2401, observe_power: 55, observe_exca_th: 6, connect_power: 70, connect_exca_th: 170, evalw: 12, fix_rate: 131, delta_range: 1, delta_cost_w: 31, atk_eval_rate: 3, },
     Param {eff: 14, key_power: 79, key_exca_th: 2401, observe_power: 75, observe_exca_th: 61, connect_power: 91, connect_exca_th: 50, evalw: 12, fix_rate: 216, delta_range: 2, delta_cost_w: 28, atk_eval_rate: 2, },
     Param {eff: 15, key_power: 109, key_exca_th: 101, observe_power: 100, observe_exca_th: 21, connect_power: 142, connect_exca_th: 20, evalw: 12, fix_rate: 176, delta_range: 1, delta_cost_w: 16, atk_eval_rate: 2, },
