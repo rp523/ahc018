ec2e20c8ca33c84b8de4f275e71cfecca1ca483b
Yusuke Goto <sample@email.com>
2023-02-26 14:58:38 +0900

    1088906.923_136113.365375

--- submit.rs
+++ submit.rs
@@ -260,9 +260,9 @@ static mut PARAM: Param = Param {
     delta_cost_w: 0,
     atk_eval_rate: 0
 };
-// 1088937.662 136117.20775
+// 1088906.923 136113.365375
 const PARAMS: [Param; 8] = [
-    Param {eff: 13, key_power: 19, key_exca_th: 1001, observe_power: 15, observe_exca_th: 71, connect_power: 16, connect_exca_th: 160, evalw: 12, fix_rate: 136, delta_range: 2, delta_cost_w: 31, atk_eval_rate: 1, },
+    Param {eff: 13, key_power: 16, key_exca_th: 651, observe_power: 15, observe_exca_th: 66, connect_power: 16, connect_exca_th: 140, evalw: 12, fix_rate: 161, delta_range: 2, delta_cost_w: 31, atk_eval_rate: 1, },
     Param {eff: 13, key_power: 22, key_exca_th: 451, observe_power: 25, observe_exca_th: 51, connect_power: 25, connect_exca_th: 100, evalw: 16, fix_rate: 81, delta_range: 1, delta_cost_w: 25, atk_eval_rate: 1, },
     Param {eff: 14, key_power: 37, key_exca_th: 951, observe_power: 25, observe_exca_th: 71, connect_power: 25, connect_exca_th: 170, evalw: 12, fix_rate: 131, delta_range: 2, delta_cost_w: 31, atk_eval_rate: 1, },
     Param {eff: 13, key_power: 31, key_exca_th: 301, observe_power: 50, observe_exca_th: 1, connect_power: 34, connect_exca_th: 10, evalw: 12, fix_rate: 131, delta_range: 2, delta_cost_w: 28, atk_eval_rate: 2, },
